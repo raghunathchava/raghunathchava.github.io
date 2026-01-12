c3074f35e93cfde52261f1f10ed7f6b2cd1cd928
Raghunath Chava <raghunath@buildworks.ai>
2026-01-12 20:10:36 +0530

    Add file aliases for all bundle manifest references

---
+++ assets/check-CJV-Dacy-CcnJqR9g.js
@@ -0,0 +1,6 @@
+import{r as o}from"./index-BaChaFcm.js";/**
+ * @license lucide-react v0.454.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=o("Check",[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]]);export{t as o};
---
+++ assets/circle-check-big-BCpKfB87-DQ8g3Xst-DnVPj7HV-BbjTYvkl.js
@@ -0,0 +1,6 @@
+import{r as e}from"./index-BaChaFcm.js";/**
+ * @license lucide-react v0.454.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=e("CircleCheckBig",[["path",{d:"M21.801 10A10 10 0 1 1 17 3.335",key:"yps3ct"}],["path",{d:"m9 11 3 3L22 4",key:"1pflzl"}]]);export{r as e};
